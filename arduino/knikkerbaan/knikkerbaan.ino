3df833495762f5cd21531f13c782454f34c3cab7
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2024-01-16 11:56:18 +0100

    Update knikkerbaan.ino

--- arduino/knikkerbaan/knikkerbaan.ino
+++ arduino/knikkerbaan/knikkerbaan.ino
@@ -1,5 +1,11 @@
 #include "src/Teller.cpp"
 #include "src/WiFiNINA/WiFiNINA.h"
+#include "src/Firebase/Firebase_Arduino_WiFiNINA.h"
+
+#define DATABASE_URL "kbaan-3c854-default-rtdb.europe-west1.firebasedatabase.app"  //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_SECRET "WiMVzQCOn3Z337zmGjcCjAxsCOEad9VHurWLDje4"
+#define WIFI_SSID "knikkerbaan"
+#define WIFI_PASSWORD "roodblauwgeelwit"
 
 int status = WL_IDLE_STATUS;
 
@@ -8,6 +14,7 @@ const int TELLER_A_PIN = 5;  // pin waarop IR-sensor voor Teller A is aangeslote
 unsigned long laatsteTellerPrintTijd = 0;
 const int TELLERPRINTINTERVAL = 1000;  // 1000 milliseconden
 
+FirebaseData fbdo;  // database object
 Teller tellerA = Teller(TELLER_A_PIN);
 
 void setup() {
@@ -32,7 +39,8 @@ void setup() {
 
   Serial.println("Connected to WiFi");
 
-  
+  Firebase.begin(DATABASE_URL, DATABASE_SECRET, WIFI_SSID, WIFI_PASSWORD);
+  Firebase.reconnectWiFi(true);
 }
 
 
@@ -44,6 +52,25 @@ void loop() {
     // print het getelde aantal knikkers
     Serial.print("Getelde knikkers: ");
     Serial.println(tellerA.getAantal());
+
+    // stuur aantal knikkers naar database:
+    if (Firebase.setInt(fbdo, "teams/team everest/aantalKnikkers", tellerA.getAantal())) {
+      Serial.println("FB set gelukt!");
+    } else {
+      Serial.println("FB set niet gelukt!");
+      Serial.println(fbdo.errorReason());
+    }
+
+    // haal 'supergetal' op van database
+    if (Firebase.getInt(fbdo, "teams/team everest/supergetal")) {
+      Serial.println("FB get gelukt!");
+      int supergetal = fbdo.intData();
+      Serial.print("supergetal uit db is: ");
+      Serial.println(supergetal);
+    } else {
+      Serial.println("FB set niet gelukt!");
+      Serial.println(fbdo.errorReason());
+    }
 
     // stel de tijd waarop voor het laatst geprint
     // werd in op 'nu'
